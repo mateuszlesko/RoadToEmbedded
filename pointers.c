28fcae4d8ad2da4f007ff56fd9157d4f11252ab5
leskers606 <leskers606@localhost.localdomain>
2021-09-13 20:53:54 +0200

    char array pointer

--- pointers.c
+++ pointers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <ctype.h>
 /*
 THEORY:
 Pointers is a variable that store address of another variable / value
 We can use it as a reference to another value, variable in RAM.
 */
 
-//passing by value
+//passing by the value
 int whatIsMore(int a, int b){
     if(a == b){
         return 0;
@@ -17,7 +18,7 @@ int whatIsMore(int a, int b){
     return -1;
 }
 
-//passing by reference
+//passing by the reference
 void swap(int* a, int* b){
     int tmp = (*a);
     *a = *b;
@@ -25,6 +26,7 @@ void swap(int* a, int* b){
 }
 
 /*Passing array as a function parameter is simply pass a pointer to the first element of an array*/
+/*but it doesn't mean that arrays and pointers are the same types*/
 int sum(int A[], int size){
     int i = 0;
     int sum = 0;
@@ -36,6 +38,13 @@ int sum(int A[], int size){
         i++;
     }
     return sum;
+}
+
+void string2Upper(char* A){
+    while(*A != '\0'){
+        *A = toupper((*A));
+        A++;
+    }
 }
 
 int main(){
@@ -52,7 +61,28 @@ int main(){
     printf("Value of variable 17 = %d \n", *pnt17);
     printf("Value of variable 13 = %d \n", *pnt13);
 
-    int arr[] = {5,5,5};
+    /*
+    HOW MANY BYTES FOR DATA TYPES:
+    int = 4 bytes
+    float = 4 bytes
+    char = 1 bytes
+    */
+
+    /*We can cast one pointer to another type*/
+    int bigInt = 601;
+    int* d = &bigInt;
+    char* pnt2Char = (char*)d;
+    //Char is stored as 1byte and int as 4bytes so if we cast int to char, then the orginal value is changing
+    printf("Casted value of int pointer to char = %d \n", *pnt2Char); 
+    int arr[] = {1,5,8};
+    printf("Address of begining of array = %d \n", *arr);
+    //so the next element is at position = first + 4 bytes 
+    printf("Address of the next element = %d \n",*(arr+1));
     int sumOfArr = sum(arr,(sizeof(arr)/sizeof(int)));
-    printf("Sum of elements is %d",sumOfArr);
+    printf("Sum of elements is %d \n",sumOfArr);
+
+    char name[]= "Mateusz";
+    printf("My name is %s; the count of letters is %d \n",name, strlen(name));
+    string2Upper(name);
+    printf("My name is %s \n",name);
 }
