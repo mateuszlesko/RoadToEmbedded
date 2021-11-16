6ffd2fd3eefa5ab5100f760802022a8bc8b05306
mateuszlesko <mateusz.lesko@outlook.com>
2021-11-16 12:34:14 -0500

    binary search tree node deletion

--- binaryTree.c
+++ binaryTree.c
@@ -8,9 +8,7 @@ struct Node{
 };
 struct Node* root;
 int elements = 0;
-void print(){
 
-}
 void insert(int value){
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->data = value;
@@ -73,12 +71,64 @@ struct Node* search(int value){
     }
 }
 
+void swap(struct Node* a, struct Node* b){
+    struct Node* tmp = a;
+    a->data = b->data;
+    b->data = tmp->data;
+}
+
+struct Node* searchMinValueInRightSide(){
+    if(root != NULL){
+        struct Node* tmp = (struct Node*) malloc(sizeof(struct Node));
+        tmp = root;
+        while(tmp->rightChild->rightChild != NULL){
+            tmp = tmp->rightChild;
+        }
+        return  tmp->leftChild != NULL ? tmp->leftChild : NULL;
+    }
+    return NULL;
+
+}
+
+void removeFromTree(int value){
+    struct Node* node2Delete = search(value);
+
+    //the last elements
+    if(node2Delete->leftChild == NULL && node2Delete->rightChild == NULL)
+        free(node2Delete);
+    //have one child
+    else if(node2Delete->leftChild != NULL && node2Delete->rightChild == NULL){
+        swap(node2Delete,node2Delete->leftChild);
+        free(node2Delete->leftChild);
+    }
+    //have one child
+    else if(node2Delete->leftChild == NULL && node2Delete->rightChild != NULL){
+        swap(node2Delete,node2Delete->rightChild);
+        free(node2Delete->rightChild);
+    }
+    //have both child
+    else{
+        struct Node* tmp = searchMinValueInRightSide();
+        swap(root,tmp);
+        free(tmp);
+    }
+}
+
 int main(void){
   struct Node* find = search(1);
   printf("%d \n",find == NULL);
-  insert(7);
-  insert(9);
-  insert(1);
-  find = search(10);
-  printf("%d \n",find == NULL);
+  insert(50);
+  insert(40);
+  insert(70);
+  insert(60);
+  insert(80);
+  find = search(80);
+  printf("80 before deletion %d \n",find == NULL);
+  removeFromTree(80);
+  find = search(80);
+  printf("80 after deletion %d \n",find == NULL);
+  printf("root before deletion %d \n",root->data);
+  //remove root
+  removeFromTree(50);
+  printf("root before deletion %d \n",root->data); 
 }
